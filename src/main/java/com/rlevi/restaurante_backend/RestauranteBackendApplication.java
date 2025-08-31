fa04ca2a6be959d4355ddb1e8775502e1c6676cc
rlevidev <leviizinho765@gmail.com>
2025-08-30 23:40:29 -0300

    first commit

---
+++ src/main/java/com/rlevi/restaurante_backend/RestauranteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rlevi.restaurante_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestauranteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestauranteBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rlevi/restaurante_backend/RestauranteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rlevi.restaurante_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestauranteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
