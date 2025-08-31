83186fe5f0d291237062f25a62c1366cb080b1d8
Raí Levi <137456612+rlevidev@users.noreply.github.com>
2025-08-31 12:19:14 -0300

    feat: adicionar novas informações no Response de pedidos

--- src/main/java/com/rlevi/restaurante_backend/dto/PedidosResponseDTO.java
+++ src/main/java/com/rlevi/restaurante_backend/dto/PedidosResponseDTO.java
@@ -1,6 +1,10 @@
 package com.rlevi.restaurante_backend.dto;
 
+import java.time.LocalDateTime;
+
 import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,9 +13,16 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
+@Data
 public class PedidosResponseDTO {
     private Long pedidoId;
     private String nomeAlimento;
-    private Double precoTotal;
+    private Double precoUnitario;
     private Integer quantidade;
+    private Double precoTotal;
+    private String nomeCliente;
+    private String enderecoCliente;
+    private String telefoneCliente;
+    private LocalDateTime dataCriacao;
 }
