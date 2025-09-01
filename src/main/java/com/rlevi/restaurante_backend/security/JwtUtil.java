59130dcc7fc666ebe9383c4629609eaed38e645c
Raí Levi <137456612+rlevidev@users.noreply.github.com>
2025-09-01 10:34:25 -0300

    feat: implementar utilitário para manipulação JWT

---
+++ src/main/java/com/rlevi/restaurante_backend/security/JwtUtil.java
@@ -0,0 +1,39 @@
+package com.rlevi.restaurante_backend.security;
+
+import java.security.Key;
+import java.util.Date;
+
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+@Component
+public class JwtUtil {
+    private final String SECRET = "uma_chave_super_secreta_para_assinar_tokens"; 
+    private final long EXPIRATION = 1000 * 60 * 60; // 1h
+
+    private Key getSigningKey() {
+        return Keys.hmacShaKeyFor(SECRET.getBytes());
+    }
+
+    public String generateToken(String username, String role) {
+        return Jwts.builder()
+                .setSubject(username)
+                .claim("role", role)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public Claims extractClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
