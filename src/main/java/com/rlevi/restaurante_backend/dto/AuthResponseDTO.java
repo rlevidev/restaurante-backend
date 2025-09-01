533a0021c76ace7500aa00dcd4878d153efffeb4
Raí Levi <137456612+rlevidev@users.noreply.github.com>
2025-09-01 10:35:26 -0300

    feat: criar DTO de resposta de autenticação

---
+++ src/main/java/com/rlevi/restaurante_backend/dto/AuthResponseDTO.java
@@ -0,0 +1,16 @@
+package com.rlevi.restaurante_backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class AuthResponseDTO {
+    private String token;
+}
