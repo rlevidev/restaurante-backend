1067aeb9e263753a2052a7aea6048525fa2c2fe2
Raí Levi <137456612+rlevidev@users.noreply.github.com>
2025-09-05 18:51:26 -0300

    feat: adicionar camada de acesso a dados de Usuários ( refs #1 )

---
+++ src/main/java/com/rlevi/restaurante_backend/repository/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package com.rlevi.restaurante_backend.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rlevi.restaurante_backend.model.Usuarios;
+
+public interface UsuarioRepository extends JpaRepository<Usuarios, Long> {
+    Optional<Usuarios> findByNome(String nome);
+}
