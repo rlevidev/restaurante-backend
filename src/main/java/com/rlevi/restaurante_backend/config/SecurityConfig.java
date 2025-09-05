ffc0fdf4e8cb6e85a80da4f9daf3dc38842e52e7
Raí Levi <137456612+rlevidev@users.noreply.github.com>
2025-09-05 18:55:36 -0300

    refactor: modificiar a segurança dos Endpoints ( refs #1 )

--- src/main/java/com/rlevi/restaurante_backend/config/SecurityConfig.java
+++ src/main/java/com/rlevi/restaurante_backend/config/SecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -27,12 +29,18 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authorize -> authorize
-                                .requestMatchers("/auth/login", "/alimentos/listar", "/alimentos/buscar/{id}", "/pedidos/criar").permitAll()
+                                .requestMatchers("/auth/register", "/auth/login").permitAll()
+                                .requestMatchers("/alimentos/listar", "/alimentos/buscar/{id}", "/pedidos/criar").hasRole("USER")
                                 .requestMatchers("/alimentos/deletar/{id}", "/alimentos/atualizar/{id}", "/pedidos/deletar/{id}", "/pedidos/listar").hasRole("ADMIN")
                                 .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
