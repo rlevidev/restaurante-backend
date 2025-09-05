23eee885718e043ae133b9783d6efc356e8404ea
Raí Levi <137456612+rlevidev@users.noreply.github.com>
2025-09-05 18:54:13 -0300

    fix: adicionar construtor padrão ao model de Usuário ( refs #1 )

--- src/main/java/com/rlevi/restaurante_backend/model/Usuarios.java
+++ src/main/java/com/rlevi/restaurante_backend/model/Usuarios.java
@@ -5,12 +5,16 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "usuarios")
 public class Usuarios {
     @Id
