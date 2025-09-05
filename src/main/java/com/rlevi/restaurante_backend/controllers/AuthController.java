8bdfcde74759e6eb10f89b6dc92edd72a3607c4a
Raí Levi <137456612+rlevidev@users.noreply.github.com>
2025-09-05 18:54:57 -0300

    fix: adicionar os Endpoints necessários para o sistema de Login e Registro ( refs #1 )

--- src/main/java/com/rlevi/restaurante_backend/controllers/AuthController.java
+++ src/main/java/com/rlevi/restaurante_backend/controllers/AuthController.java
@@ -1,31 +1,47 @@
 package com.rlevi.restaurante_backend.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.rlevi.restaurante_backend.dto.AuthRequestDTO;
-import com.rlevi.restaurante_backend.dto.AuthResponseDTO;
+import com.rlevi.restaurante_backend.dto.LoginRequestDTO;
+import com.rlevi.restaurante_backend.dto.LoginResponseDTO;
+import com.rlevi.restaurante_backend.dto.RegisterRequestDTO;
+import com.rlevi.restaurante_backend.model.Usuarios;
+import com.rlevi.restaurante_backend.repository.UsuarioRepository;
 import com.rlevi.restaurante_backend.security.JwtUtil;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+    @Autowired
+    private JwtUtil jwtUtil;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
-    private final JwtUtil jwtUtil;
+    @PostMapping("/register")
+    public ResponseEntity<LoginResponseDTO> register(@RequestBody RegisterRequestDTO registerRequest){
+        Usuarios usuarios = usuarioRepository.save(new Usuarios(null, registerRequest.email(), registerRequest.nome(), passwordEncoder.encode(registerRequest.senha()), "ROLE_USER"));
 
-    public AuthController(JwtUtil jwtUtil) {
-        this.jwtUtil = jwtUtil;
+        String token = jwtUtil.generateToken(usuarios.getNome(), usuarios.getRole());
+        return ResponseEntity.ok(new LoginResponseDTO(token));
     }
 
     @PostMapping("/login")
-    public AuthResponseDTO login(@RequestBody AuthRequestDTO request) {
-        if ("admin".equals(request.getUsername()) && "admin123".equals(request.getPassword())) {
-            String token = jwtUtil.generateToken(request.getUsername(), "ADMIN");
-            return new AuthResponseDTO(token);
-        }
-        throw new RuntimeException("Credenciais inválidas");
-    }
+    public ResponseEntity<LoginResponseDTO> login(@RequestBody LoginRequestDTO loginRequest){
+        Usuarios usuarios = usuarioRepository.findByNome(loginRequest.nome()).orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
 
+        if (!passwordEncoder.matches(loginRequest.senha(), usuarios.getSenha())) {
+            throw new RuntimeException("Senha inválida");
+        }
+
+        String token = jwtUtil.generateToken(usuarios.getNome(), usuarios.getRole());
+        return ResponseEntity.ok(new LoginResponseDTO(token));
+    }
 }
