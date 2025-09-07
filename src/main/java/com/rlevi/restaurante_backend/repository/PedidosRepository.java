dc00c113859023fce99da3d90970cf1c05a95b1d
Raí Levi <137456612+rlevidev@users.noreply.github.com>
2025-09-07 14:44:20 -0300

    feat: adicicionar metodo de listar pedidos pelo usuário ( refs #2 )

--- src/main/java/com/rlevi/restaurante_backend/repository/PedidosRepository.java
+++ src/main/java/com/rlevi/restaurante_backend/repository/PedidosRepository.java
@@ -1,11 +1,16 @@
 package com.rlevi.restaurante_backend.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.rlevi.restaurante_backend.model.Pedidos;
+import com.rlevi.restaurante_backend.model.Usuarios;
 
 @Repository
 public interface PedidosRepository extends JpaRepository<Pedidos, Long>{
+
+    List<Pedidos> findByUsuarioOrderByDataCriacaoDesc(Usuarios usuario);
 
 }
