ddd9b265568b9a63ae31c90a236bdb3543b55517
Raí Levi <137456612+rlevidev@users.noreply.github.com>
2025-09-07 16:18:33 -0300

    fix: role Admin pode acessar todos os endpoints ( refs #2 )

--- src/main/java/com/rlevi/restaurante_backend/config/SecurityConfig.java
+++ src/main/java/com/rlevi/restaurante_backend/config/SecurityConfig.java
@@ -1,37 +1,45 @@
 package com.rlevi.restaurante_backend.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 import com.rlevi.restaurante_backend.security.JwtFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    @Autowired
+    
     private final JwtFilter jwtFilter;
+    private final UserDetailsService userDetailsService;
 
-    public SecurityConfig(JwtFilter jwtFilter) {
+    public SecurityConfig(JwtFilter jwtFilter, UserDetailsService userDetailsService) {
         this.jwtFilter = jwtFilter;
+        this.userDetailsService = userDetailsService;
     }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                                 .requestMatchers("/auth/register", "/auth/login").permitAll()
-                                .requestMatchers("/alimentos/listar", "/alimentos/buscar/{id}", "/pedidos/criar").hasRole("USER")
-                                .requestMatchers("/alimentos/deletar/{id}", "/alimentos/atualizar/{id}", "/pedidos/deletar/{id}", "/pedidos/listar").hasRole("ADMIN")
+                                .requestMatchers("/alimentos/listar", "/alimentos/buscar/{id}").hasAnyRole("USER", "ADMIN")
+                                .requestMatchers("/pedidos/criar").hasAnyRole("USER", "ADMIN")
+                                .requestMatchers("/alimentos/**", "/pedidos/**").hasRole("ADMIN")
                                 .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
@@ -43,7 +51,16 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
     }
 }
