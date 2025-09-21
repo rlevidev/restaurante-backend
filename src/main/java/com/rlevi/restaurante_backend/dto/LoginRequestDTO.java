0bf833b6f7418375c7e9f465d1a7a28935402626
Raí Levi <137456612+rlevidev@users.noreply.github.com>
2025-09-21 12:26:05 -0300

    refactor: adicionar validação nos dados de login ( refs #5 )

--- src/main/java/com/rlevi/restaurante_backend/dto/LoginRequestDTO.java
+++ src/main/java/com/rlevi/restaurante_backend/dto/LoginRequestDTO.java
@@ -1,3 +1,10 @@
 package com.rlevi.restaurante_backend.dto;
 
-public record LoginRequestDTO(String nome, String senha) {}
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record LoginRequestDTO(
+        @NotBlank(message = "Nome é obrigatório") @Size(min = 3, max = 50, message = "Nome deve ter entre 3 e 50 caracteres") String nome,
+
+        @NotBlank(message = "Senha é obrigatória") @Size(min = 6, message = "Senha deve ter pelo menos 6 caracteres") String senha) {
+}
