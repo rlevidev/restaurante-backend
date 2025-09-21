13469e1412c4246b18ea6924fa6defbb4f7bf906
Raí Levi <137456612+rlevidev@users.noreply.github.com>
2025-09-21 13:48:10 -0300

    feat: adicionar a entidade ItemPedido ( refs #3 )

---
+++ src/main/java/com/rlevi/restaurante_backend/model/ItemPedido.java
@@ -0,0 +1,48 @@
+package com.rlevi.restaurante_backend.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "itens_pedido")
+public class ItemPedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idItem;
+
+    @ManyToOne
+    @JoinColumn(name = "pedido_id", nullable = false)
+    private Pedidos pedido;
+
+    @ManyToOne
+    @JoinColumn(name = "alimento_id", nullable = false)
+    private Alimentos alimento;
+
+    @Column(nullable = false)
+    private Integer quantidade;
+
+    @Column(name = "preco_unitario", nullable = false, precision = 10, scale = 2)
+    private BigDecimal precoUnitario;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal subtotal;
+}
