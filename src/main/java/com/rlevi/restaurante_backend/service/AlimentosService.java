f1c9c53b7c14df0975b6fbb02d8b1892204248b5
Raí Levi <137456612+rlevidev@users.noreply.github.com>
2025-09-21 12:33:08 -0300

    refactor: controllers podem retornar objetos criados/atualizados e usa exceções específicas ( refs #5 )

--- src/main/java/com/rlevi/restaurante_backend/service/AlimentosService.java
+++ src/main/java/com/rlevi/restaurante_backend/service/AlimentosService.java
@@ -1,11 +1,12 @@
 package com.rlevi.restaurante_backend.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rlevi.restaurante_backend.exception.ResourceNotFoundException;
 import com.rlevi.restaurante_backend.model.Alimentos;
 import com.rlevi.restaurante_backend.repository.AlimentosRepository;
 
@@ -14,19 +15,19 @@ public class AlimentosService {
     @Autowired
     private AlimentosRepository alimentosRepository;
 
-    public void criarAlimento(Alimentos alimentos) {
-        alimentosRepository.save(alimentos);
+    public Alimentos criarAlimento(Alimentos alimentos) {
+        return alimentosRepository.save(alimentos);
     }
 
-    public void atualizarAlimento(Long id, Alimentos alimentosAtualizados) {
-        alimentosRepository.findById(id).map(alimentoExistente -> {
+    public Alimentos atualizarAlimento(Long id, Alimentos alimentosAtualizados) {
+        return alimentosRepository.findById(id).map(alimentoExistente -> {
             alimentoExistente.setNomeAlimento(alimentosAtualizados.getNomeAlimento());
             alimentoExistente.setPrecoAlimento(alimentosAtualizados.getPrecoAlimento());
             alimentoExistente.setDescricaoAlimento(alimentosAtualizados.getDescricaoAlimento());
 
             return alimentosRepository.save(alimentoExistente);
         })
-        .orElseThrow(() -> new RuntimeException("Alimento nao encontrado"));
+                .orElseThrow(() -> new ResourceNotFoundException("Alimento", id));
     }
 
     public List<Alimentos> listarAlimentos() {
