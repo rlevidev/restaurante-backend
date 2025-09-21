0243a14baf46e5e7c910c531c8b4e6e50216bad4
Raí Levi <137456612+rlevidev@users.noreply.github.com>
2025-09-21 12:26:49 -0300

    refactor: adicionar validação no cadastro ( refs #5 )

--- src/main/java/com/rlevi/restaurante_backend/dto/RegisterRequestDTO.java
+++ src/main/java/com/rlevi/restaurante_backend/dto/RegisterRequestDTO.java
@@ -1,4 +1,13 @@
 package com.rlevi.restaurante_backend.dto;
 
-public record RegisterRequestDTO(String email, String nome, String senha) {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record RegisterRequestDTO(
+        @NotBlank(message = "Email é obrigatório") @Email(message = "Email deve ter um formato válido") @Size(max = 100, message = "Email não pode exceder 100 caracteres") String email,
+
+        @NotBlank(message = "Nome é obrigatório") @Size(min = 3, max = 50, message = "Nome deve ter entre 3 e 50 caracteres") String nome,
+
+        @NotBlank(message = "Senha é obrigatória") @Size(min = 6, max = 100, message = "Senha deve ter entre 6 e 100 caracteres") String senha) {
 }
