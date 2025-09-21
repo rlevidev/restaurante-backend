c22dabcbeb84ede0a6062a580168f0046d1841e6
Raí Levi <137456612+rlevidev@users.noreply.github.com>
2025-09-21 13:48:52 -0300

    refactor: atualizar a entidade Pedidos para suportar múltiplos itens ( refs #3 )

--- src/main/java/com/rlevi/restaurante_backend/model/Pedidos.java
+++ src/main/java/com/rlevi/restaurante_backend/model/Pedidos.java
@@ -2,16 +2,20 @@ package com.rlevi.restaurante_backend.model;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
@@ -38,12 +42,8 @@ public class Pedidos {
     @JoinColumn(name = "usuario_id")
     private Usuarios usuario;
 
-    @ManyToOne
-    @JoinColumn(name = "alimento_id")
-    private Alimentos idAlimento;
-
-    @Column(nullable = false)
-    private Integer quantidade;
+    @OneToMany(mappedBy = "pedido", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<ItemPedido> itens;
 
     @Column(name = "preco_total", precision = 10, scale = 2)
     private BigDecimal precoTotal;
