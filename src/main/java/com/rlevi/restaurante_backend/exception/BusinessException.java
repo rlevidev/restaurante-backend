19b908568536454fee2c3ea1d90aa1f5ea1798d5
Raí Levi <137456612+rlevidev@users.noreply.github.com>
2025-09-21 12:22:56 -0300

    feat: adicionar exceção para regras de negócio violadas ( refs #5 )
    Centraliza erros de lógica de negócio

---
+++ src/main/java/com/rlevi/restaurante_backend/exception/BusinessException.java
@@ -0,0 +1,11 @@
+package com.rlevi.restaurante_backend.exception;
+
+public class BusinessException extends RuntimeException {
+    public BusinessException(String message) {
+        super(message);
+    }
+
+    public BusinessException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
