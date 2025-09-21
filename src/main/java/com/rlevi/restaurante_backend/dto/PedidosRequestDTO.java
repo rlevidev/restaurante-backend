05121d1fc9efda0de374325a80bd95819be66847
Raí Levi <137456612+rlevidev@users.noreply.github.com>
2025-09-21 13:44:46 -0300

    refactor: refactorar DTO de pedido para suportar multiplos itens ( refs #3 )

--- src/main/java/com/rlevi/restaurante_backend/dto/PedidosRequestDTO.java
+++ src/main/java/com/rlevi/restaurante_backend/dto/PedidosRequestDTO.java
@@ -1,8 +1,10 @@
 package com.rlevi.restaurante_backend.dto;
 
-import jakarta.validation.constraints.Min;
+import java.util.List;
+
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -15,12 +17,9 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class PedidosRequestDTO {
-    @NotNull(message = "ID do alimento é obrigatório")
-    private Long idAlimento;
-
-    @NotNull(message = "Quantidade é obrigatória")
-    @Min(value = 1, message = "Quantidade deve ser pelo menos 1")
-    private Integer quantidade;
+    @NotEmpty(message = "O pedido deve conter pelo menos um alimento")
+    @Valid
+    private List<ItemPedidoRequestDTO> itens;
 
     @NotBlank(message = "Nome do cliente é obrigatório")
     @Size(min = 2, max = 100, message = "Nome do cliente deve ter entre 2 e 100 caracteres")
