f13acece729f71ce75477b78d3a4a64cee696927
rlevidev <leviizinho765@gmail.com>
2025-12-02 21:19:51 -0300

    💄 style(security): adicionar anotacoes @NonNull e reordenar imports
    - Adiciona anotações `@NonNull` aos parâmetros do método `doFilterInternal` em `JwtFilter` para indicar que não devem ser nulos, melhorando a clareza do contrato da API.
    - Reordena as declarações de importação para seguir um padrão mais consistente e legível.

--- src/main/java/com/rlevi/restaurante_backend/security/JwtFilter.java
+++ src/main/java/com/rlevi/restaurante_backend/security/JwtFilter.java
@@ -1,11 +1,9 @@
 package com.rlevi.restaurante_backend.security;
 
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,7 +11,10 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import java.io.IOException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class JwtFilter extends OncePerRequestFilter {
@@ -23,10 +24,10 @@ public class JwtFilter extends OncePerRequestFilter {
     private UserDetailsService userDetailsService;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, 
-                                    HttpServletResponse response, 
-                                    FilterChain filterChain) throws ServletException, IOException {
-        
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain) throws ServletException, IOException {
+
         String authHeader = request.getHeader("Authorization");
 
         if (authHeader != null && authHeader.startsWith("Bearer ")) {
@@ -43,7 +44,7 @@ public class JwtFilter extends OncePerRequestFilter {
                                     null,
                                     userDetails.getAuthorities()
                             );
-                    
+
                     SecurityContextHolder.getContext().setAuthentication(authentication);
                 }
             } catch (Exception e) {
