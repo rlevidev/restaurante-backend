cde9a42b1464da351cb6c69a2a0406a28d472864
rlevidev <leviizinho765@gmail.com>
2025-12-04 18:42:28 -0300

    ✨ feat(cupom): adicionar CupomRepository para gerenciamento de cupons ( refs #7 )
    - Criado o CupomRepository para gerenciar operações de persistência da entidade Cupom.
    - Estende JpaRepository para fornecer funcionalidades CRUD básicas.
    - Incluído o método findByCodeIgnoreCase para buscar um cupom pelo código, ignorando maiúsculas e minúsculas.

---
+++ src/main/java/com/rlevi/restaurante_backend/repository/CupomRepository.java
@@ -0,0 +1,11 @@
+package com.rlevi.restaurante_backend.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rlevi.restaurante_backend.domain.entities.Cupom;
+
+public interface CupomRepository extends JpaRepository<Cupom, Long>{
+    Optional<Cupom> findByCodeIgnoreCase(String code);
+}
