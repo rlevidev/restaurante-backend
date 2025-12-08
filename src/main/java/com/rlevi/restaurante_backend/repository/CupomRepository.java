a9f2bc5ecb52d36d69a0267a6bc2c32cde7f4a41
rlevidev <leviizinho765@gmail.com>
2025-12-08 01:13:42 -0300

    Revert "✨ feat(cupom): Implementa a lógica de criação de cupons com validação de código e DTOs."
    This reverts commit 5989e6ce788266cf138195242969fe2787e60506.

--- src/main/java/com/rlevi/restaurante_backend/repository/CupomRepository.java
+++ src/main/java/com/rlevi/restaurante_backend/repository/CupomRepository.java
@@ -8,5 +8,4 @@ import com.rlevi.restaurante_backend.domain.entities.Cupom;
 
 public interface CupomRepository extends JpaRepository<Cupom, Long>{
     Optional<Cupom> findByCodeIgnoreCase(String code);
-    boolean existsByCode(String code);
 }
